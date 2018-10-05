83e32faa9cad3807f2e7c36aeab8d27c395ce194
Nikki A <nikki.apostolakis@gmail.com>
2018-10-05 17:37:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    post '/pirates' do
+        @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+        erb :display_puppy
+    end
+
 
   end
 end
